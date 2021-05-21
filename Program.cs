68c86c1eb5bebc8a6c7170eb40bb9d6cac14f53c
aramos333 <armandoramos@usf.edu>
2021-05-20 21:01:29 -0400

    Thursday Update...  still working on #5.

--- Program.cs
+++ Program.cs
@@ -11,26 +11,26 @@ namespace Assignment1
         static void Main(string[] args)
         {
 
-           
+
 
             // ------------------------------------------------------------------------------------------------------------
             // Question 1
-             question_1();
+            //question_1();
 
 
             // ------------------------------------------------------------------------------------------------------------
             // Question 2
-            question_2();
+            //question_2();
 
 
             // ------------------------------------------------------------------------------------------------------------
             // Question 3
-            question_3();
+            //question_3();
 
 
             // ------------------------------------------------------------------------------------------------------------
             // Question 4
-            question_4();
+            //question_4();
 
 
             // ------------------------------------------------------------------------------------------------------------
@@ -40,12 +40,12 @@ namespace Assignment1
 
             // ------------------------------------------------------------------------------------------------------------
             // Question 6
-            question_6();
+            //question_6();
 
 
             // ------------------------------------------------------------------------------------------------------------
             // End 
-            Console.WriteLine("Press <Enter> to terminate program");
+            Console.Write("Press <Enter> to terminate program");
             Console.ReadLine();
 
         }
@@ -61,18 +61,38 @@ namespace Assignment1
             Console.WriteLine("the task for him.  He instructed you to limit the time complexity to O(n).");
             Console.WriteLine();
 
-            int first, last;
             Console.Write("Enter numeric items separated by commas: ");
             string marks_input = Console.ReadLine();
             Console.Write("Enter target numeric value (n): ");
-            string target = Console.ReadLine();
-            string[] marks = marks_input.Split(',');
-            first = Array.IndexOf(marks, target) + 1;
-            last = Array.LastIndexOf(marks, target) + 1;
-            first = (first != 0 ? first : -1);
-            last = (last != 0 ? last : -1);
-            Console.WriteLine("[" + first.ToString() + "," + last.ToString() + "]");
+            string target_input = Console.ReadLine();
+
+            int target = Convert.ToInt32(target_input);
+            string[] temp_array = marks_input.Split(',');
+            //int[] marks = new int[temp_array.Length];
+
+            int[] marks = Convert2NumericArray(temp_array);
+
+
+            //for (int i = 0; i < temp_array.Length; i++)
+            //{
+            //    try
+            //    {
+            //        marks[i] = Convert.ToInt32(temp_array[i]);
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine(e.Message);
+            //    }
+            //}
+
+            // Use targetRange()
+            int[] results = targetRange(marks, target);
+            Console.WriteLine("Results: [{0}]", string.Join(", ", results));
             Console.WriteLine();
+            Console.Write("Press <Enter> to continue...");
+            Console.ReadLine();
+
+
         }
 
 
@@ -92,14 +112,16 @@ namespace Assignment1
             string in_string = Console.ReadLine();
             char delimeter = ' ';
             // Custom SplitString function
-            string[] words = SplitString(in_string, delimeter);
-            foreach (string x in words)
+            string[] words = StringSplit(in_string, delimeter);
+            foreach (string word in words)
             {
-                // Custom ReverseString function
-                Console.Write(ReverseString(x) + delimeter);
+                // Use ReverseString()
+                Console.Write(StringReverse(word) + delimeter);
             }
             Console.WriteLine();
             Console.WriteLine();
+            Console.Write("Press <Enter> to continue...");
+            Console.ReadLine();
         }
 
 
@@ -107,16 +129,25 @@ namespace Assignment1
         public static void question_3()
         {
             Console.WriteLine("------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine("Question #3: NEEDS WORK!");
+            Console.WriteLine("Question #3:");
             Console.WriteLine("Professor Stablein is given a sorted integer array. He needs to make the array elements");
             Console.WriteLine("distinct by increasing each value as needed, while minimizing the array sum. Professor");
             Console.WriteLine("Stablein thought this was an interesting exercise that the students might enjoy completing.");
             Console.WriteLine(" Your job is to complete the method to print the minimum possible sum as output");
             Console.WriteLine();
 
-            Console.WriteLine("Enter numbers separated by commas: ");
-            Console.WriteLine("NEEDS WORK!");
+            Console.Write("Enter sorted numbers separated by commas: ");
+            string input = Console.ReadLine();
+            string[] temp_array = input.Split(',');
+            int[] numbers = new int[temp_array.Length];
+            for (int i = 0; i < numbers.Length; ++i)
+                numbers[i] = Convert.ToInt32(temp_array[i]);
+
+            // Use of minSum()
+            Console.WriteLine("The minimum sum is: " + minSum(numbers).ToString());
             Console.WriteLine();
+            Console.Write("Press <Enter> to continue...");
+            Console.ReadLine();
         }
 
 
@@ -131,37 +162,13 @@ namespace Assignment1
 
 
             Console.Write("Enter the word to assess: ");
-            string word = Console.ReadLine();     
-            Dictionary<char, int> CharacterCount = new Dictionary<char, int>();
+            string word = Console.ReadLine();
 
-            //Add character to dictionary
-            for (int i = 0; i < word.Length; i++)   
-            {
-                if (CharacterCount.ContainsKey(word[i]))  
-                {
-                    CharacterCount[word[i]]++;
-                }
-                else
-                {
-                    CharacterCount.Add(word[i], 1);   
-                }
-            }
-
-            // Order dictionary items by descending value
-            var items = from pair in CharacterCount
-                        orderby pair.Value descending
-                        select pair;
-
-            // Display results.
-            foreach (KeyValuePair<char, int> pair in items)
-            {
-                for (int i = 1; i <= pair.Value; i++)
-                {
-                    Console.Write(pair.Key);
-                }
-            }
+            // Use FreqSort()
+            Console.WriteLine(FreqSort(word));
             Console.WriteLine();
-            Console.WriteLine();
+            Console.Write("Press <Enter> to continue...");
+            Console.ReadLine();
         }
 
 
@@ -176,9 +183,31 @@ namespace Assignment1
             Console.WriteLine("Rocky with the example below.");
             Console.WriteLine();
 
-            Console.WriteLine("Enter first array of numbers separated by commas: ");
-            Console.WriteLine("NEEDS WORK!");
+            Console.Write("Enter first array of integers separated by commas: ");
+            string input_string = Console.ReadLine();
+            string[] string_array1 = input_string.Split(',');
+            int[] array1 = Convert2NumericArray(string_array1);
+
+            Console.Write("Enter second array of integers separated by commas: ");
+            input_string = Console.ReadLine();
+            string[] string_array2 = input_string.Split(',');
+            int[] array2 = Convert2NumericArray(string_array2);
+
+            // Sort numeric arrays
+            Array.Sort(array1);
+            Array.Sort(array2);
+
+            if (array1.Length <= array2.Length)
+            {
+                Console.WriteLine("Results: [{0}]", string.Join(", ", Intersect1(array1, array2)));
+            }
+            else
+            {
+                Console.WriteLine("Results: [{0}]", string.Join(", ", Intersect1(array2, array1)));
+            }
             Console.WriteLine();
+            Console.Write("Press <Enter> to continue...");
+            Console.ReadLine();
         }
 
 
@@ -192,9 +221,12 @@ namespace Assignment1
             Console.WriteLine("and the absolute difference between i and j is at most k.");
             Console.WriteLine();
 
+            // Get string to process
             Console.Write("Enter first array of characters separated by commas: ");
             string input_string = Console.ReadLine();
-            string[] temp_array = input_string.Split(',');
+            char[] char_array = input_string.Replace(",", "").ToCharArray();
+
+            // Get numeric index
             int k = 0;
             Console.Write("Enter absolute difference numeric value (k): ");
             string input_k = Console.ReadLine();
@@ -207,27 +239,32 @@ namespace Assignment1
                 Console.WriteLine(e.Message);
             }
 
-            bool response = false;
-            for (int i = 0; i < temp_array.Length - 1; i++)
-            {
-                for (int j = i + 1; j < temp_array.Length; j++)
-                {
-                    if (temp_array[i] == temp_array[j])
-                    {
-                        if ((j - i).Equals(k))
-                        {
-                            response = true;
-                        }
-                    }
-                }
-            }
+            // Assess character array
+            bool response = ContainsDuplicate(char_array, k);
             Console.WriteLine("Response is " + response.ToString());
             Console.WriteLine();
         }
-            
 
 
-        public static string[] SplitString(string in_string, char delimiter)
+
+
+
+        // Used in question #1
+        public static int[] targetRange(int[] marks, int target)
+        {
+            int[] results = new int[2];
+            results[0] = Array.IndexOf(marks, target) + 1;
+            results[1] = Array.LastIndexOf(marks, target) + 1;
+            results[0] = (results[0] != 0 ? results[0] : -1);
+            results[1] = (results[1] != 0 ? results[1] : -1);
+            return results;
+        }
+
+
+
+
+        // Used in question #2
+        public static string[] StringSplit(string in_string, char delimiter)
         {
             List<String> words = new List<String>();
             int start = 0;
@@ -247,7 +284,8 @@ namespace Assignment1
 
 
 
-        public static string ReverseString(string in_string)
+        // Used in question #2
+        public static string StringReverse(string in_string)
         {
 
             string out_string = String.Empty;
@@ -260,6 +298,225 @@ namespace Assignment1
             return out_string;
         }
 
+
+
+        // Used in question #3
+        public static int minSum(int[] arr)
+        {
+            Dictionary<int, int> MinimumSum = new Dictionary<int, int>();
+            int sum = 0;
+
+            //Add character to dictionary
+            int j = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                try
+                {
+                    j = arr[i];
+                    while (MinimumSum.ContainsKey(j))
+                    {
+                        j++;
+                    }
+                    MinimumSum.Add(j, j);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+            foreach (KeyValuePair<int, int> item in MinimumSum)
+            {
+                sum = sum + item.Value;
+            }
+
+            return sum;
+        }
+
+
+        // Used in question #4
+        public static string FreqSort(string s)
+        {
+            Dictionary<char, int> CharacterCount = new Dictionary<char, int>();
+            string output = String.Empty;
+
+            //Add character to dictionary
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (CharacterCount.ContainsKey(s[i]))
+                {
+                    CharacterCount[s[i]]++;
+                }
+                else
+                {
+                    CharacterCount.Add(s[i], 1);
+                }
+            }
+
+            // Order dictionary items by descending value
+            var items = from pair in CharacterCount
+                        orderby pair.Value descending
+                        select pair;
+
+            // compile output string
+            foreach (KeyValuePair<char, int> pair in items)
+            {
+                for (int i = 1; i <= pair.Value; i++)
+                {
+                    output = output + pair.Key;
+                }
+            }
+
+            return output;
+        }
+
+
+        // Used in question #5
+        public static int[] Intersect0(int[] nums1, int[] nums2)
+        {
+            var results = nums1.Intersect(nums2);
+            return (int[])results;
+        }
+
+
+        public static int[] Intersect1(int[] nums1, int[] nums2)
+        {
+            int[] results = new int[nums1.Length];
+
+            // Initial pointers and results for merge check.
+            int i = 0;
+            int j = 0;
+            int value = 0;
+
+            // Continue until either array is exhausted.
+            while ((i < nums1.Length) & (j < nums2.Length ))
+                {
+                    //Check for intersect or which list has lowest value.
+
+                    if (nums1[i] == nums2[j])
+                    {
+                    //Intersect, save, advance both lists to next number.
+
+                        results[i] = nums1[i];
+                        value = nums1[i];
+
+                        while ((i < nums1.Length) & ( nums1[i] == value))
+                        {
+                            i++;
+                        }
+                            
+                        while ((j < nums2.Length) & (nums2[j] == value))
+                        {
+                            j++;
+                        }
+                            
+                    }
+                    else if (nums1[i] < nums2[j])
+                    {
+                        //# A has smallest, advance it to next number.
+                        value = nums1[i];
+                        while (( i < nums1.Length) & ( nums1[i] == value))
+                            {
+                                i++;
+                            }
+                            
+                    }
+                    else
+                    {
+                        //# B has smallest, advance it to next number.
+                        value = nums2[j];
+                    while ((j < nums2.Length) & (nums2[j] == value))
+                        j++;
+                    }
+                        
+                }
+                
+
+
+            return results;
+        }
+
+
+
+        public static int[] Intersect2(int[] nums1, int[] nums2)
+        {
+           
+            Dictionary<int, bool> Intersection = new Dictionary<int, bool>();
+            int sum = 0;
+
+            //Add smaller array items to dictionary
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                if (!Intersection.ContainsKey(nums1[i]))
+                {
+                    Intersection.Add(nums1[i], false);
+                }   
+            }
+
+            //Process larger array items against dictionary
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                if (!Intersection.ContainsKey(nums2[i]))
+                {
+                    Intersection[nums2[i]] = true;
+                }
+            }
+
+            int intersection_count = Intersection.Count(kv => kv.Value.Equals(true));
+            int[] results = new int[intersection_count];
+            int i = -1;
+            foreach (KeyValuePair<int, bool> item in Intersection)
+            {
+                if (item.Value.Equals(true))
+                {
+                    i++;
+                    results[i] = item.Key;
+                }
+            }
+            return results;
+
+        }
+
+
+        // Used in question #6
+        public static bool ContainsDuplicate(char[] arr, int k)
+        {
+            bool response = false;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        if ((j - i).Equals(k))
+                        {
+                            response = true;
+                        }
+                    }
+                }
+            }
+
+            return response;
+        }
+
+
+        public static int[] Convert2NumericArray(string[] input)
+        {
+            int[] results = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                try
+                {
+                    results[i] = Convert.ToInt32(input[i]);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return results;
+        }
 
 
     }
